080a59e5072897807e496726c037d12df0ae3f22
EC2 Default User <ec2-user@ip-172-31-42-207.ap-northeast-1.compute.internal>
2023-10-04 23:49:44 +0000

    [Finish] ouyou-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   devise_for :users
-  resources :users, only: [:index,:show,:edit,:update]
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
 
-
+  resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
